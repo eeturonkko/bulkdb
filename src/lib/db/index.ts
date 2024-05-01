d67005ea685e7f9b0343eeef988f32f4e61cb76a
Eetu Rönkkö <eetu.kristian.ronkko@gmail.com>
2024-05-01 14:51:56 +0300

    Update database connection in src/lib/db/index.ts

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,10 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
-import { env } from '$env/dynamic/private';
 import * as schema from './schema';
-import { DATABASE_URL } from '$env/static/private';
+import dotenv from 'dotenv';
+dotenv.config();
 
 // for query purposes
-const queryClient = postgres(DATABASE_URL);
+const queryClient = postgres(process.env.DATABASE_URL);
 export const db = drizzle(queryClient, { schema });
