c6c5f23b1a0ea54d1d92ffda0a1edfb364c22685
Eetu Rönkkö <eetu.kristian.ronkko@gmail.com>
2024-05-19 14:08:41 +0300

    Order weeks by ID in layout server load

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,8 +1,12 @@
 import type { LayoutServerLoad } from './$types';
 import { db } from '$lib/db/index';
+import { asc } from 'drizzle-orm';
+import { weeks } from '$lib/db/schema';
 
 export const load: LayoutServerLoad = async () => {
 	return {
-		weeks: await db.query.weeks.findMany()
+		weeks: await db.query.weeks.findMany({
+			orderBy: [asc(weeks.id)]
+		})
 	};
 };
