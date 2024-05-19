803f7461c8f3639a15cf5418e21c36f26a5d333a
Eetu Rönkkö <eetu.kristian.ronkko@gmail.com>
2024-05-19 11:48:15 +0300

    chore: Add d3 chart for average weights and labels in +page.svelte

--- src/routes/analytics/+page.svelte
+++ src/routes/analytics/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import * as d3 from 'd3';
-
 	interface AverageWeight {
 		week_name: string;
 		average_weight: string;
@@ -14,20 +13,38 @@
 	export let data: PageData;
 
 	const avgWeights: number[] = data.averageWeights.map((d) => parseFloat(d.average_weight));
+	const labels: string[] = data.averageWeights.map((d) => d.week_name);
 
 	let el: HTMLDivElement;
 
 	onMount(() => {
-		d3.select(el)
+		const chart = d3.select(el);
+
+		const bar = chart
 			.selectAll('div')
 			.data(avgWeights)
 			.enter()
 			.append('div')
 			.attr('class', 'bg-blue-500 text-white font-semibold px-1 my-0.5 text-start')
 			.style('width', (d: number) => `${d * 10}px`)
+			.style('position', 'relative')
 			.text((d: number) => d.toString());
+
+		bar
+			.append('div')
+			.data(labels)
+			.attr('class', 'label text-white text-sm')
+			.style('position', 'absolute')
+			.style('left', (d: string) => '-120px') // Adjust this value as needed to move the labels
+			.style('top', '50%')
+			.style('transform', 'translateY(-50%)')
+			.text((d: string) => d);
 	});
 </script>
+
+<svelte:head>
+	<title>Bulkdb | Analytics</title>
+</svelte:head>
 
 <main class="flex flex-col items-center space-y-1">
 	<h2 class="text-xl font-bold">Analytics</h2>
