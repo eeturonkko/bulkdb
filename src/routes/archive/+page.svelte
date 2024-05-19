c53c084d0601695e77b81d62db7fd735b76facfd
Eetu Rönkkö <eetu.kristian.ronkko@gmail.com>
2024-05-19 11:56:00 +0300

    add archive page with some dummy data for testing purposes

---
+++ src/routes/archive/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import * as Table from '$lib/components/ui/table';
+
+	//export let data: PageData;
+</script>
+
+<svelte:head>
+	<title>Bulkdb | Archive</title>
+</svelte:head>
+
+<main class="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6">
+	<div class="flex items-center justify-between"></div>
+	<div class="rounded-lg border shadow-sm">
+		<Table.Root>
+			<Table.Header>
+				<Table.Row>
+					<Table.Head>Week</Table.Head>
+					<Table.Head>Actions</Table.Head>
+				</Table.Row>
+			</Table.Header>
+			<Table.Body>
+				<Table.Row>
+					<Table.Cell>
+						<a href="/week/2">Week 1 test</a>
+					</Table.Cell>
+				</Table.Row>
+				<Table.Row>
+					<Table.Cell>
+						<a href="/week/61">Week 2 test</a>
+					</Table.Cell>
+				</Table.Row>
+			</Table.Body>
+		</Table.Root>
+	</div>
+</main>
