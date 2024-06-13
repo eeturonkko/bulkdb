81c2c0bbea5887ac12a3d233979574fa62581f9b
Eetu Rönkkö <eetu.kristian.ronkko@gmail.com>
2024-06-13 17:43:42 +0300

    Update Navlist width

--- src/lib/components/Navlist.svelte
+++ src/lib/components/Navlist.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
-	import { ScrollArea } from '$lib/components/ui/scroll-area/index.js';
-	import { Separator } from '$lib/components/ui/separator/index.js';
-	import ThemeToggleButton from './ThemeToggleButton.svelte';
 	import { weeksStore } from '../../store';
+	import ThemeToggleButton from './ThemeToggleButton.svelte';
+	import { Separator } from '$lib/components/ui/separator/index.js';
+	import { ScrollArea } from '$lib/components/ui/scroll-area/index.js';
 	import { CalendarDays, Home, AreaChart, Archive, Dumbbell } from 'lucide-svelte';
 
 	$: weeksStores = weeksStore;
 </script>
 
 <ScrollArea
-	class="sm:w-23 fixed inset-y-0 left-0 z-10 hidden w-48 flex-col border-r bg-background sm:flex"
+	class="sm:w-23 fixed inset-y-0 left-0 z-10 hidden w-52 flex-col border-r bg-background sm:flex"
 >
 	<div class="p-4">
 		<div class="flex flex-col items-start justify-center gap-4">
